f75e5ff70a87b50ffe4def6efcfe65da33f4a9b4
Keonnartey <125210401+Keonnartey@users.noreply.github.com>
2023-10-11 17:56:48 +0000

    editted the caesar package.

--- caesar-cipher-cli/src/main.rs
+++ caesar-cipher-cli/src/main.rs
@@ -11,39 +11,71 @@ cargo run --  --message "Ypp dy dro lexuob. Ofobi zobcyx pyb drowcovfoc" --decry
 */
 
 
-use caeser_cipher_cli::{decrypt, encrypt};
-use clap::Parser;
+// use caeser_cipher_cli::{decrypt, encrypt};
+// use clap::Parser;
 
-/// CLI tool to encrypt and decrypt messages using the caeser cipher
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Encrypt the message
-    #[arg(short, long)]
-    encrypt: bool,
+// /// CLI tool to encrypt and decrypt messages using the caeser cipher
+// #[derive(Parser, Debug)]
+// #[command(author, version, about, long_about = None)]
+// struct Args {
+//     /// Encrypt the message
+//     #[arg(short, long)]
+//     encrypt: bool,
 
-    /// decrypt the message
-    #[arg(short, long)]
-    decrypt: bool,
+//     /// decrypt the message
+//     #[arg(short, long)]
+//     decrypt: bool,
 
-    /// The message to encrypt or decrypt
-    #[arg(short, long)]
-    message: String,
+//     /// The message to encrypt or decrypt
+//     #[arg(short, long)]
+//     message: String,
 
-    /// The shift to use for the cipher
-    /// Must be between 1 and 25, the default is 3
-    #[arg(short, long, default_value = "3")]
-    shift: u8,
+//     /// The shift to use for the cipher
+//     /// Must be between 1 and 25, the default is 3
+//     #[arg(short, long, default_value = "3")]
+//     shift: u8,
+// }
+
+// // run it
+// fn main() {
+//     let args = Args::parse();
+//     if args.encrypt {
+//         println!("{}", encrypt(&args.message, args.shift));
+//     } else if args.decrypt {
+//         println!("{}", decrypt(&args.message, args.shift));
+//     } else {
+//         println!("Please specify either --encrypt or --decrypt");
+//     }
+// }
+
+
+pub fn encrypt(text: &str, shift: u8) -> String {
+    let mut result = String::new();
+    for c in text.chars() {
+        if c.is_ascii_alphabetic() {
+            let base = if c.is_ascii_lowercase() { b'a' } else { b'A' };
+            let offset = (c as u8 - base + shift) % 26;
+            result.push((base + offset) as char);
+        } else {
+            result.push(c);
+        }
+    }
+    result
 }
 
-// run it
+pub fn decrypt(text: &str, shift: u8) -> String {
+    encrypt(text, 26 - shift)
+}
+
 fn main() {
-    let args = Args::parse();
-    if args.encrypt {
-        println!("{}", encrypt(&args.message, args.shift));
-    } else if args.decrypt {
-        println!("{}", decrypt(&args.message, args.shift));
-    } else {
-        println!("Please specify either --encrypt or --decrypt");
-    }
+    let plaintext = "The quick brown fox jumps over the lazy dog.";
+    let shift = 5;
+
+    let ciphertext = encrypt(plaintext, shift);
+    let decrypted_text = decrypt(&ciphertext, shift);
+
+    println!("Plaintext: {}", plaintext);
+    println!("Shift: {}", shift);
+    println!("Ciphertext: {}", ciphertext);
+    println!("Decrypted text: {}", decrypted_text);
 }
